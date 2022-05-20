cba8fb26f0d23b8bdf563fe2f907eff9e821a16d
karansrawat <karansrawat09@gmail.com>
2022-05-20 15:22:36 +0530

    code for color change

---
+++ javascript.js
@@ -0,0 +1,15 @@
+var div = document.getElementById("color");
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeColor(){
+  div.style.backgroundColor= getRandomColor();
+}
+setInterval(changeColor,1000);
---
+++ readme.md
@@ -0,0 +1,2 @@
+# color changer after one Milliosecond
+code for a HTML page which will change color after one millisecond.
